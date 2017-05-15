8e9f9b86bf6c96bfa749a0fa62d5245bad0c8430
Oleg Kuperman <cyiwork@outlook.com>
2017-05-15 12:20:17 +0300

    CRUD operations
    Added database connection

---
+++ config/databases.php
@@ -0,0 +1,54 @@
+<?php
+	// 1. Create a database connection
+	$dbhost = "localhost";
+	$dbuser = "my_app";
+	$dbpass = "secret";
+	$dbname = "my_app";
+	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// Test if connection occured.
+	if(mysqli_Connect_errno()) {
+		die("Database connection failed: " .
+			mysqli_connect_error() .
+			" (" . mysqli_Connect_errno() . ")"
+		);
+	}
+?>
+
+<?php
+	// 2. Perform database query
+	$query = "SELECT * FROM user_records";
+	$result = mysqli_query($connection, $query);
+	// Test if there was a query error
+	if (!$result) {
+		die("Database quesry failed.");
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Databases</title>
+	</head>
+	<body>
+	<?php
+	  //3. Use returned data (if any)
+	  while($row = mysqli_fetch_row($result)) {
+		// output data from each row
+		var_dump($row);
+		echo "<hr />";
+	  }
+	?>
+
+	<?php
+	  // 4. Release returned data
+	  mysqli_free_result($result);
+	?>
+
+	</body>
+</html>
+
+<?php
+	// 5. Close Database Connection
+	mysqli_close($connection);
+?>
